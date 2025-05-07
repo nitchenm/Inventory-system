31d6c81180dc7646a78c51a824fc8502091b2c0f
nitchenm <nitchenm@gmail.com>
2025-05-07 02:00:23 -0400

    Se añaden metodos a InvMovCont

--- src/main/java/com/grupoestudio/Inventory_system/controller/InventoryMovementController.java
+++ src/main/java/com/grupoestudio/Inventory_system/controller/InventoryMovementController.java
@@ -2,28 +2,24 @@ package com.grupoestudio.Inventory_system.controller;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.grupoestudio.Inventory_system.model.InventoryMovement;
-import com.grupoestudio.Inventory_system.repository.InventoryMovementRepository;
-import com.grupoestudio.Inventory_system.service.InventoryMovementService;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grupoestudio.Inventory_system.model.InventoryMovement;
+import com.grupoestudio.Inventory_system.service.InventoryMovementService;
 
 
 
 @RestController
-@RequestMapping("/api/v1/inventory-movement")
+@RequestMapping("/api/v1/inventory-movements")
 public class InventoryMovementController {
 
     @Autowired
@@ -63,11 +59,18 @@ public class InventoryMovementController {
             return ResponseEntity.status(HttpStatus.CREATED).body(invToSave);
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
-        
-        
+        }
     }
-    
-    
+
+    @GetMapping("/{sourceid}/transfer-stock/{targetid}")
+    public ResponseEntity<Void> transferStock(@RequestParam Long sourceid, @RequestParam Long targetid, int quantity) {
+        try {
+            invMovService.transferStock(sourceid, targetid, quantity);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
     
 
 }
